9ec7432a96c7ea2177d65807e6d800a0a0f7dd9a
jsxgod <smykoo7@gmail.com>
2021-01-20 03:39:38 +0100

    Add basic socket io functionality: monitoring socket connection and disconnection

--- server/index.js
+++ server/index.js
@@ -10,6 +10,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+/**
+    Acts as a "main" for this file because we have to constatly monitor
+    the {socket} of connected client
+*/
+io.on('connection', (socket) => {
+    console.log('New client connection...')
+
+    
+    socket.on('disconnect', () => {
+        console.log('Client disconnected...')
+    })
+});
+
 app.use('/', router);
 
 server.listen(PORT, () => console.log(`server started on port ${PORT}`));
