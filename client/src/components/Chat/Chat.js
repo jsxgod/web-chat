6ee7db031ac7c777b5b08ee7339a09eace9dee4b
jsxgod <smykoo7@gmail.com>
2021-01-20 05:07:49 +0100

    Create hooks, parse data from the link's parameters and send it to the backend with socket's emit.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 import './Chat.css';
 
-const Chat = () => {
+let socket;
+
+const Chat = (props) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:4000';
+
+    useEffect(() => {
+        /* Location is coming from React Router as a prop*/
+        const parsedData = queryString.parse(props.location.search);
+
+        socket = io(ENDPOINT)
+        
+        setName(parsedData.name);
+        setRoom(parsedData.room);
+
+        socket.emit('onJoin', { name, room });
+    }, [ENDPOINT, props.location.search])
+    
     return (
         <h1>hi from Chat component</h1>
     )
 }
 
-export default Chat
+export default Chat;
