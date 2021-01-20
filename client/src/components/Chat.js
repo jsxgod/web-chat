8bed46da56279863fbe15d91e6d48c0ce3e5bf97
jsxgod <smykoo7@gmail.com>
2021-01-20 03:11:26 +0100

    Create Home and Chat components

---
+++ client/src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chat = () => {
+    return (
+        <h1>hi from Chat component</h1>
+    )
+}
+
+export default Chat
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <h1>hi from Home component</h1>
+    )
+}
+
+export default Home
