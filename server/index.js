bcaa1e1065ed71e79ddc81ed1887e26bb4a18fc6
jsxgod <smykoo7@gmail.com>
2021-01-20 03:20:14 +0100

    Initialize basic server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 4000
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+server.listen(PORT, () => console.log(`server started on port ${PORT}`));
