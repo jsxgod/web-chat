2c02e7c3e4746f93e50b9ee8e7b527c354999e4b
jsxgod <smykoo7@gmail.com>
2021-01-20 23:29:05 +0100

    Create chat window components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 export {default as Home} from './Home/Home';
 
 export {default as Chat} from './Chat/Chat';
+
+export {default as InfoBar} from './InfoBar/InfoBar';
+
+export {default as MessageInput} from './MessageInput/MessageInput';
