826d3f5824ca757dc55d59f0919c004487447e19
jsxgod <smykoo7@gmail.com>
2021-01-20 04:18:59 +0100

    Add site structure. Create hooks for the login form.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <h1>hi from Home component</h1>
+        <div className="container">
+            <div className="login-container">
+                <h1 className="heading">Welcome to WebChat</h1>
+                <div><input className="login-input" placeholder="Name" type="text" onChange={(event) => setName(event.target.value)} /></div>
+                <div><input className="login-input mt-20" placeholder="Room" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="login-button mt-20" type="submit">Join</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
